0df5be9ff33ebfe39a2601b10f8accfb1491b584
Leonnardo Lair Tavares Gomes <leonnardo0316@gmail.com>
2022-06-10 20:53:32 -0300

    post lapidando

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const drinks = [
 app.post('/recipes', function (req, res) {
     const { id, name, price } = req.body;
     recipes.push({ id, name, price});
-    res.status(201).json({ message: 'Recipe created successfully!'});
+    res.status(201).json({ message: `Recipe ${name} created successfully!`});
   });
 
 app.get('/recipes', function (req, res) {
