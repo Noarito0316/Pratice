13017ca0b0a214c4c7a552090e10ebf18ec215f3
Leonnardo Lair Tavares Gomes <leonnardo0316@gmail.com>
2022-06-10 19:31:23 -0300

    praticing

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express(); // 1
+
+app.get('/hello', handleHelloWorldRequest); // 2
+app.get('/', mainPage); // 2
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+}); // 3
+
+function handleHelloWorldRequest(req, res) {
+    res.status(200).send('Olá mundo!!!!');
+  }
+
+function mainPage(req, res) {
+    res.status(200).send('Main Page!'); // 4
+  }
