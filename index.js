aeced4e7f29afd4c876ce1858439b7e7b5ed3bc4
Leonnardo Lair Tavares Gomes <leonnardo0316@gmail.com>
2022-06-10 19:51:00 -0300

    Search by ID

--- index.js
+++ index.js
@@ -25,6 +25,25 @@ app.get('/drinks', function (req, res) {
     res.json(drinks);
   });
 
+  app.get('/recipes/:id', function (req, res) {
+    const { id } = req.params;
+    const recipe = recipes.find((r) => r.id === Number(id));
+  
+    if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+  
+    res.status(200).json(recipe);
+  });
+
+  app.get('/drinks/:id', function (req, res) {
+    const { id } = req.params;
+    const drink = drinks.find((r) => r.id === Number(id));
+  
+    if (!drink) return res.status(404).json({ message: 'Drink not found!'});
+  
+    res.status(200).json(drink);
+  });
+
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
